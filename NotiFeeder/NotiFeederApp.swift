5806adaae9dac97a957eb6eef6e105c788d56ac7
Dyonisos Fergadiotis <dyonisos.fergadiotis@mail.de>
2025-11-04 13:14:22 +0100

    Initial Commit

---
+++ NotiFeeder/NotiFeederApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiFeederApp.swift
+//  NotiFeeder
+//
+//  Created by Dyonisos Fergadiotis on 04.11.25.
+//
+
+import SwiftUI
+
+@main
+struct NotiFeederApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
